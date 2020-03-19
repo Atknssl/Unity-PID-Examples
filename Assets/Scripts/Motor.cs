1d7b6af52ca6591f71ac3bf3e909210384acb2ba
CilginPortakal <atakansuslu@hotmail.com>
2020-03-19 18:58:17 +0300

    First Commit
    Hello World

---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float force = 0;
+    private void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        _rb.AddRelativeForce(Vector2.up*force);
+    }
+}
---
+++ Assets/Scripts/PID.cs
@@ -0,0 +1,23 @@
+[System.Serializable]
+public class PID {
+	public float pFactor, iFactor, dFactor;
+		
+	float integral;
+	float lastError;
+	
+	
+	public PID(float pFactor, float iFactor, float dFactor) {
+		this.pFactor = pFactor;
+		this.iFactor = iFactor;
+		this.dFactor = dFactor;
+	}
+	
+	
+	public float Update(float setpoint, float actual, float timeFrame) {
+		float present = setpoint - actual;
+		integral += present * timeFrame;
+		float deriv = (present - lastError) / timeFrame;
+		lastError = present;
+		return present * pFactor + integral * iFactor + deriv * dFactor;
+	}
+}
