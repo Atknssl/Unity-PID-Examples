f8ef44d0ccd23c72725b5be630df116c1aec78ed
atknssl <atakan.suslu@metu.edu.tr>
2020-10-14 18:04:33 +0300

    Main Menu

---
+++ Assets/Scenes/Main Menu/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadBalance()
+    {
+         SceneManager.LoadScene("Balance Rod");
+    }
+
+    public void LoadAltitude()
+    {
+         SceneManager.LoadScene("Altitude Control");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
